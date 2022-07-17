3ea682e6c4bd26f04066034c7f6985cd9e50797d
Domaa2022 <victormiguelbarahonalicona@gmail.com>
2022-07-17 01:04:25 -0600

    primer avance

---
+++ js/controlador.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('form');
+const input = document.querySelectorAll('#form input');
+
+
+
+function enviarPaginaAdmin() {
+    window.location.href = "webAdministrativa.html";
+}
